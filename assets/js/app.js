28d0b3c4bdfddfe2f7513864cb85b40eed7dd3ea
blurrcat <blurrcat@gmail.com>
2012-10-28 22:27:52 +0800

    Change source structure

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-/*
- * app.js
- * Copyright (C) 2012 blurrcat <blurrcat@gmail.com>
- *
- * Distributed under terms of the MIT license.
- */
-
-
---
+++ public/assets/js/app.js
@@ -0,0 +1,20 @@
+/*
+ * app.js
+ * Copyright (C) 2012 blurrcat <blurrcat@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+(function($) {
+    
+    $(document).bind("mobileinit", function() {
+        $.mobile.allowCrossDomainPages = true;
+        $.mobile.defaultPageTransition = "slide";
+        $.mobile.page.prototype.options.addBackBtn = true;
+        $.mobile.page.prototype.options.headerTheme = "d";
+        $.mobile.page.prototype.options.footerTheme = "d";
+        $.mobile.dialog.prototype.options.headerTheme = "d";
+        $.mobile.listview.prototype.options.dividerTheme = "d";
+    });
+
+}(jQuery));
